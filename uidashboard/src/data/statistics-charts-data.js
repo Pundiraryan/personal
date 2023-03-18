dd9c43747046b2c354903677e0ce489cfa737b2a
Govind Jangra <govind70821@gmail.com>
2023-03-19 03:20:24 +0530

    commit

--- uidashboard/src/data/statistics-charts-data.js
+++ uidashboard/src/data/statistics-charts-data.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { chartsConfig } from "@/configs";
 
 // const websiteViewsChart = {
@@ -101,59 +100,125 @@
 
 import axios from "axios";
 
-=======
 import { chartsConfig } from "@/configs";
->>>>>>> 58dfcce3c0530890ea511ed9fcc9dba7a4ff0aa5
+
+
+
+
+
+
 const websiteViewsChart = {
-  type: "bar",
-  height: 220,
-  series: [
-    {
-      name: "Views",
-      data: [1000, 20, 10, 22, 50, 10, 40],
+    type: "bar",
+    height: 220,
+    series: [
+      {
+        name: "Views",
+        data: [],
+      },
+    ],
+    options: {
+      ...chartsConfig,
+      colors: "#fff",
+      plotOptions: {
+        bar: {
+          columnWidth: "16%",
+          borderRadius: 5,
+        },
+      },
+      xaxis: {
+        ...chartsConfig.xaxis,
+        categories: [],
+      },
     },
-  ],
-  options: {
-    // chart options
-  },
-};
-
-const dailySalesChart = {
-  type: "line",
-  height: 220,
-  series: [
-    {
-      name: "Sales",
-      data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
+  };
+  
+  const dailySalesChart = {
+    type: "line",
+    height: 220,
+    series: [
+      {
+        name: "Sales",
+        data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
+      },
+    ],
+    options: {
+      ...chartsConfig,
+      colors: ["#fff"],
+      stroke: {
+        lineCap: "round",
+      },
+      markers: {
+        size: 5,
+      },
+      xaxis: {
+        ...chartsConfig.xaxis,
+        categories: [
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+      },
     },
-  ],
-  options: {
-    // chart options
-  },
-};
+  };
+  
+  const completedTasksChart = {
+    ...dailySalesChart,
+    series: [
+      {
+        name: "Tasks",
+        data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+      },
+    ],
+  };
+  let overall = [];
 
-const completedTasksChart = {
-  ...dailySalesChart,
-  series: [
-    {
-      name: "Tasks",
-      data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
-    },
-  ],
-};
-
-let statisticsChartsData = [];
 
 async function fetchData() {
   try {
-    const response = await axios.get("http://localhost:4000/careplaceDetails/govind4");
-    statisticsChartsData = response.data;
-    console.log
+    const response = await axios.get("http://localhost:4000/careplaceDetails/all");
+    overall = response.data;
+    const categories = overall.map((data) => data.supid);
+    const staffsize = overall.map((data) => data.staffsize);
+    
+    console.log(categories);
+    console.log(staffsize);
+    websiteViewsChart.options.xaxis.categories = categories;
+    websiteViewsChart.series[0].data = staffsize;
   } catch (error) {
     console.log(error);
   }
 }
 
 fetchData();
-
-export { statisticsChartsData };
+  export const statisticsChartsData = [
+    {
+      color: "blue",
+      title: "Food stats",
+      description: "Last Campaign Performance",
+      footer: "updated recently",
+      chart: websiteViewsChart,
+    },
+    {
+      color: "pink",
+      title: "Daily Sales",
+      description: "15% increase in today sales",
+      footer: "updated 4 min ago",
+      chart: dailySalesChart,
+    },
+    {
+      color: "green",
+      title: "Completed Tasks",
+      description: "Last Campaign Performance",
+      footer: "just updated",
+      chart: completedTasksChart,
+    },
+  ];
+  
+ 
+export default statisticsChartsData;
