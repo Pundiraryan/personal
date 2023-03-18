8230ba821f33aa9d1b0c335c5aca4cd3c6fb051c
Govind Jangra <govind70821@gmail.com>
2023-03-19 02:33:31 +0530

    commit

--- uidashboard/src/data/statistics-charts-data.js
+++ uidashboard/src/data/statistics-charts-data.js
@@ -1,4 +1,104 @@
-import { chartsConfig } from "@/configs";
+// import { chartsConfig } from "@/configs";
+
+// const websiteViewsChart = {
+//   type: "bar",
+//   height: 220,
+//   series: [
+//     {
+//       name: "Views",
+//       data: [1000, 20, 10, 22, 50, 10, 40],
+//     },
+//   ],
+//   options: {
+//     ...chartsConfig,
+//     colors: "#fff",
+//     plotOptions: {
+//       bar: {
+//         columnWidth: "16%",
+//         borderRadius: 5,
+//       },
+//     },
+//     xaxis: {
+//       ...chartsConfig.xaxis,
+//       categories: ["M", "T", "W", "T", "F", "S", "S"],
+//     },
+//   },
+// };
+
+// const dailySalesChart = {
+//   type: "line",
+//   height: 220,
+//   series: [
+//     {
+//       name: "Sales",
+//       data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
+//     },
+//   ],
+//   options: {
+//     ...chartsConfig,
+//     colors: ["#fff"],
+//     stroke: {
+//       lineCap: "round",
+//     },
+//     markers: {
+//       size: 5,
+//     },
+//     xaxis: {
+//       ...chartsConfig.xaxis,
+//       categories: [
+//         "Apr",
+//         "May",
+//         "Jun",
+//         "Jul",
+//         "Aug",
+//         "Sep",
+//         "Oct",
+//         "Nov",
+//         "Dec",
+//       ],
+//     },
+//   },
+// };
+
+// const completedTasksChart = {
+//   ...dailySalesChart,
+//   series: [
+//     {
+//       name: "Tasks",
+//       data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+//     },
+//   ],
+// };
+
+// export const statisticsChartsData = [
+//   {
+//     color: "blue",
+//     title: "Food stats",
+//     description: "Last Campaign Performance",
+//     footer: "updated recently",
+//     chart: websiteViewsChart,
+//   },
+//   {
+//     color: "pink",
+//     title: "Daily Sales",
+//     description: "15% increase in today sales",
+//     footer: "updated 4 min ago",
+//     chart: dailySalesChart,
+//   },
+//   {
+//     color: "green",
+//     title: "Completed Tasks",
+//     description: "Last Campaign Performance",
+//     footer: "just updated",
+//     chart: completedTasksChart,
+//   },
+// ];
+
+// export default statisticsChartsData;
+
+
+
+import axios from "axios";
 
 const websiteViewsChart = {
   type: "bar",
@@ -10,18 +110,7 @@ const websiteViewsChart = {
     },
   ],
   options: {
-    ...chartsConfig,
-    colors: "#fff",
-    plotOptions: {
-      bar: {
-        columnWidth: "16%",
-        borderRadius: 5,
-      },
-    },
-    xaxis: {
-      ...chartsConfig.xaxis,
-      categories: ["M", "T", "W", "T", "F", "S", "S"],
-    },
+    // chart options
   },
 };
 
@@ -35,28 +124,7 @@ const dailySalesChart = {
     },
   ],
   options: {
-    ...chartsConfig,
-    colors: ["#fff"],
-    stroke: {
-      lineCap: "round",
-    },
-    markers: {
-      size: 5,
-    },
-    xaxis: {
-      ...chartsConfig.xaxis,
-      categories: [
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-      ],
-    },
+    // chart options
   },
 };
 
@@ -70,28 +138,18 @@ const completedTasksChart = {
   ],
 };
 
-export const statisticsChartsData = [
-  {
-    color: "blue",
-    title: "Food stats",
-    description: "Last Campaign Performance",
-    footer: "updated recently",
-    chart: websiteViewsChart,
-  },
-  {
-    color: "pink",
-    title: "Daily Sales",
-    description: "15% increase in today sales",
-    footer: "updated 4 min ago",
-    chart: dailySalesChart,
-  },
-  {
-    color: "green",
-    title: "Completed Tasks",
-    description: "Last Campaign Performance",
-    footer: "just updated",
-    chart: completedTasksChart,
-  },
-];
+let statisticsChartsData = [];
 
-export default statisticsChartsData;
+async function fetchData() {
+  try {
+    const response = await axios.get("http://localhost:4000/careplaceDetails/govind4");
+    statisticsChartsData = response.data;
+    console.log
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchData();
+
+export { statisticsChartsData };
